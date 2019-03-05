9c6675053a4ffe5551fc5d304d01e7db8237ed3c
SivakumarVayyeti <sivakumar.vayyeti@gmail.com>
2019-03-04 23:06:00 -0500

    Models classes have been added for the items-category sections

--- routes/defaultRouter.js
+++ routes/defaultRouter.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const importItemUtility = require('../utility/itemDataStore');
+
+const getItems = importItemUtility.getItems;
+const getItem = importItemUtility.getItem;
+const getItemsByCategory = importItemUtility.getItemsByCategory;
 
 const router = express.Router();
 
@@ -7,15 +12,46 @@ router.get('/', (request, response) => {
 });
 
 router.get('/catagories', (request, response) => {
-  response.render('catagories.ejs');
+  const data = getItemsByCategory();
+  response.render('catagories.ejs', {"catalogData": data});
 });
 
 router.get('/item-details', (request, response) => {
-  response.render('item-details.ejs');
+  const requestParams = request.query;
+  itemcode = requestParams['item-code'];
+  const itemObj = getItem(itemcode);
+  if(typeof itemObj === "undefined"){
+    response.redirect('/catagories');
+  }
+  else{
+    response.render('item-details.ejs', {"item":itemObj});
+  }
 });
 
 router.get('/my-items', (request, response) => {
   response.render('my-items.ejs');
+});
+
+router.get('/feedback', (request, response) => {
+  const requestParams = request.query;
+  itemcode = requestParams['item-code'];
+  const itemObj = getItem(itemcode);
+
+  if(typeof itemObj === "undefined"){
+    response.redirect('/catagories');
+  }
+
+  else {
+    response.render('feedback.ejs', {"item":itemObj});
+  }
+});
+
+router.get('/about', (request, response) => {
+  response.render('about.ejs');
+});
+
+router.get('/contact', (request, response) => {
+  response.render('contact.ejs');
 });
 
 module.exports = router;
